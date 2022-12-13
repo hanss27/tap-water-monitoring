a5b374568f5dec0a835aa27e2c26f22808e33e0d
hanss27 <raihan.muhammad91@ui.ac.id>
2022-12-14 03:18:08 +0700

    feat: Add Images, Header, and Footer

--- pages/top.js
+++ pages/top.js
@@ -1,28 +1,119 @@
 import clientPromise from "../lib/mongodb";
 
-export default function Top({ water_datas }) {
+// import logo from "../resources/logo512.png";
+
+export default function Top({ sensor }) {
   return (
+
     <div>
-      <h1>Top 1000 Movies of All Time</h1>
-      <p>
-        <small>(According to Metacritic)</small>
-      </p>
-      <ul>
-            {water_datas.map((water_data) => (
-                    <li>
-                        <p>Tes</p>
+      <div className="Header"
+        style={{
+          padding: '0',
+          backgroundColor:  'rgb(0, 0, 0)',
+          textAlign: 'center',
+          position:'fixed',
+          top: 0,
+          right: 0,
+          left: 0,
+          
+          height : '15%',
+          width: '100%',
+        }}>
+        <img src="/logo512.png" alt="Vercel Logo" style={{
+          float:'right',
+          height:'50px',
+          width:'50px',
+          backgroundSize: 'contain',
+          backgroundPositionX:'right',
+          margin: '0 0 0 0px',
+          }} >
+        </img>
+        
+        <h1 style={{
+            
+        }}>Welcome!</h1>
+
+        <p>Kel 2.14: Tap Water Monitoring System</p>
+        <style jsx>{`
+        h1{
+            color: rgb(255,255,255);
+            margin-top: 10px;
+            margin-bottom:0px;
+            padding-top: 5px;
+            padding-bottom:0px;
+          }
+        p{
+            color:rgb(255,255,255);
+            padding-top: 10px;
+            margin-top: 0px;
+        }
+            `}</style>
+      </div>
+      <div className = "Body"
+        style={{
+          textAlign: 'center'
+        }}>
+          <h1>
+            Kondisi Tap Water Saat Ini
+          </h1>
+          <ul>
+            {sensor.map((water_data) => (
+                    <li style= {{listStyleType:'none'}}>
                         <h2>{water_data.time}</h2>
                         <h3>{water_data.PH}</h3>
                         <p>{water_data.Turbudity}</p>
                         <p>{water_data.TDS}</p>
                         <p>{water_data.Temp}</p>
-
-
                     </li>
             ))}
-      </ul>
+          </ul>
+          <style jsx>{`
+        h1{
+          margin: 100px 0px 0px 0px;
+        }
+        h2{
+          
+        }
+        h3{
+
+        }
+        p{
+            color: rgb(0,0,0);
+            margin-top: 10px;
+            margin-bottom:0px;
+            padding-top: 5px;
+            padding-bottom:0px;
+          }
+        ul{
+            color:rgb(0,0,0);
+            padding-top: 10px;
+            margin-top: 0px;
+        }
+
+            `}</style>
+        </div>
+      
+
+          <div className="footer"
+          style={{
+            backgroundColor: 'rgb(235, 195, 64)',
+            textAlign: 'center',
+            position:'fixed',
+            bottom: 0,
+            right: 0,
+            left: 0,
+            width: "100%"
+          }}>
+          <p>Created by Desain Proyek Teknik Elektro 2019 Kelompok 2.14</p>
+        </div>
+
+
+
     </div>
-  );
+
+    
+    
+  )
 }
 
 export async function getStaticProps() {
@@ -30,14 +121,14 @@ export async function getStaticProps() {
         const client = await clientPromise;
         const db = client.db("watermonitoring");
 
-       const water_datas = await db
+       const sensor = await db
            .collection("waterdatabase")
            .find({})
            .sort({ time: 1 })
            .limit(10)
            .toArray();
         return {
-            props: { water_datas: JSON.parse(JSON.stringify(water_datas)) },
+            props: { sensor: JSON.parse(JSON.stringify(sensor)) },
         };
     } catch (e) {
         console.error(e);
