06e802c95d3b58eebd9669a1f39c3e6603f64574
hanss27 <raihan.muhammad91@ui.ac.id>
2022-12-19 13:06:46 +0700

    fix: still fixing datetime

--- pages/api/tes.js
+++ pages/api/tes.js
@@ -7,13 +7,20 @@ export default async (req, res) => {
        const db = client.db("watermonitoring");
        switch (req.method){
         case "POST":
-            var x = new Date();
-            var x1=x.toUTCString();// changing the display to UTC string
-          
-            x = convertUTCDateToLocalDate(x);
-            var month = x.getMonth() + 1;
-            var x1=x.getFullYear() + "/" + month + "/" + x.getDate() + "/" + x.getHours() + ":" + x.getMinutes(); 
-            req.body.time = x1;
+            var x = new Date().toLocaleString("en-US", {timeZone: "Asia/Jakarta"})
+            console.log(x)
+            let readDate = x.split(",")
+            let readTime = readDate[1].split(":") 
+            const timeZone = readTime[2];
+            let hours;
+            if (timeZone.includes('PM')){
+                hours = parseInt(readTime[0])+12;
+            }
+            else{
+                hours = parseInt(readTime[0]);
+            }
+            var curTime = readDate[0] + "/" + hours + ":" + readTime[1];
+            req.body.time = curTime;
             let myPost = await db.collection("waterdatabase").insertOne(req.body);
             res.status(201).json(myPost);
             break;                      
@@ -33,14 +40,3 @@ export default async (req, res) => {
    }
 };
 
-
-function convertUTCDateToLocalDate(date) {
-    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
-
-    var offset = date.getTimezoneOffset() / 60;
-    var hours = date.getHours();
-
-    newDate.setHours(hours - offset);
-
-    return newDate;   
-}
