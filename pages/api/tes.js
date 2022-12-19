66aafbc2ab4aa877d1f0a54c1d4dee08b2f9d976
hanss27 <raihan.muhammad91@ui.ac.id>
2022-12-19 12:31:01 +0700

    feat: RTC data automated

--- pages/api/tes.js
+++ pages/api/tes.js
@@ -1,15 +1,18 @@
 import clientPromise from "../../lib/mongodb";
-
+"2022/12/25/11:30"
 export default async (req, res) => {
    try {
        const client = await clientPromise;
+      
        const db = client.db("watermonitoring");
        switch (req.method){
         case "POST":
-            let bodyObject = JSON.parse(JSON.stringify(req.body));
-            console.log(bodyObject);
-            let myPost = await db.collection("waterdatabase").insertOne(bodyObject);
-            res.json(myPost.ops[0]);
+            var x = new Date()
+            var x1=x.toUTCString();// changing the display to UTC string
+            var x1=x.getFullYear() + "/" + x.getMonth() + "/" + x.getDate() + "/" + x.getHours() + ":" + x.getMinutes(); 
+            req.body.time = x1;
+            let myPost = await db.collection("waterdatabase").insertOne(req.body);
+            res.status(201).json(myPost);
             break;                      
         case "GET":
             const water_data = await db
